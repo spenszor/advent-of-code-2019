37f8521e07da9e6857bbc78e10aa5f0410d5be18
spenszor <slawosz.penszor@gmail.com>
2019-12-03 08:56:02 +0100

    day 3 still wip

--- day03.py
+++ day03.py
@@ -1,44 +1,49 @@
 
 def make_map(grid, raw_wire, symbol):
-    pos_x = 2500
-    pos_y = 2500
+    pos_x = 5000
+    pos_y = 5000
     for d in raw_wire:
         direction = d[0]
         length = int(d[1:])
         if direction == 'U':
             for y in range(pos_y, length):
                 pos_y += 1
-                check(grid[pos_x][pos_y], symbol)
+                s = check(grid[pos_x][pos_y], symbol)
+                grid[pos_x][pos_y] = s
         elif direction == 'D':
             for y in range(pos_y, length):
                 pos_y -= 1
-                check(grid[pos_x][pos_y], symbol)
+                s = check(grid[pos_x][pos_y], symbol)
+                grid[pos_x][pos_y] = s
         elif direction == 'L':
             for x in range(pos_x, length):
                 pos_x -= 1
-                check(grid[pos_x][pos_y], symbol)
+                s = check(grid[pos_x][pos_y], symbol)
+                grid[pos_x][pos_y] = s
         else:
             for x in range(pos_x, length):
                 pos_x += 1
-                check(grid[pos_x][pos_y], symbol)
+                s = check(grid[pos_x][pos_y], symbol)
+                grid[pos_x][pos_y] = s
 
 def check(value, current_symbol):
     if value is not None and value != current_symbol:
-        value = 'X'
+        print('got cross')
+        return 'X'
     else:
-        value = current_symbol
+        return current_symbol
 
 with open('./inputs/day03') as input:
     split_wire1 = input.readline().split(',')
-    grid = [[None for _ in range(5000)] for _ in range(5000)]
+    grid = [[None for _ in range(10000)] for _ in range(10000)]
     make_map(grid, split_wire1, 'w1')
     split_wire2 = input.readline().split(',')
     make_map(grid, split_wire2, 'w2')
     min_distance = 10000000
-    for x in range(5000):
-        for y in range(5000):
+    for x in range(10000):
+        for y in range(10000):
             if grid[x][y] == 'X':
-                dist = abs(2500 - x) + abs(2500 - y)
+                dist = abs(5000 - x) + abs(5000 - y)
                 if dist < min_distance:
                     min_distance = dist
     print(min_distance)
