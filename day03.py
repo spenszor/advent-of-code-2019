8f9d758899e9caa6f55274a94a87922268c3de3e
Slawosz Penszor <s.penszor@ocado.com>
2019-12-03 12:59:14 +0100

    Day thrid first part solution

--- day03.py
+++ day03.py
@@ -1,49 +1,54 @@
+center_x = 15000
+center_y = 15000
+max_size = 30000
+
 
 def make_map(grid, raw_wire, symbol):
-    pos_x = 5000
-    pos_y = 5000
+    pos_x = center_x
+    pos_y = center_y
     for d in raw_wire:
         direction = d[0]
         length = int(d[1:])
         if direction == 'U':
-            for y in range(pos_y, length):
+            for y in range(pos_y, pos_y + length):
                 pos_y += 1
                 s = check(grid[pos_x][pos_y], symbol)
                 grid[pos_x][pos_y] = s
         elif direction == 'D':
-            for y in range(pos_y, length):
+            for y in range(pos_y, pos_y + length):
                 pos_y -= 1
                 s = check(grid[pos_x][pos_y], symbol)
                 grid[pos_x][pos_y] = s
         elif direction == 'L':
-            for x in range(pos_x, length):
+            for x in range(pos_x, pos_x + length):
                 pos_x -= 1
                 s = check(grid[pos_x][pos_y], symbol)
                 grid[pos_x][pos_y] = s
         else:
-            for x in range(pos_x, length):
+            for x in range(pos_x, pos_x + length):
                 pos_x += 1
                 s = check(grid[pos_x][pos_y], symbol)
                 grid[pos_x][pos_y] = s
+    return grid
 
 def check(value, current_symbol):
-    if value is not None and value != current_symbol:
-        print('got cross')
+    if value != None and value != current_symbol:
         return 'X'
     else:
         return current_symbol
 
 with open('./inputs/day03') as input:
     split_wire1 = input.readline().split(',')
-    grid = [[None for _ in range(10000)] for _ in range(10000)]
-    make_map(grid, split_wire1, 'w1')
+    grid = [[None for _ in range(max_size)] for _ in range(max_size)]
+    grid = make_map(grid, split_wire1, 'w1')
+    print(filter(lambda x: x != None, grid[center_x]))
     split_wire2 = input.readline().split(',')
-    make_map(grid, split_wire2, 'w2')
+    grid = make_map(grid, split_wire2, 'w2')
     min_distance = 10000000
-    for x in range(10000):
-        for y in range(10000):
+    for x in range(max_size):
+        for y in range(max_size):
             if grid[x][y] == 'X':
-                dist = abs(5000 - x) + abs(5000 - y)
-                if dist < min_distance:
+                dist = abs(center_x - x) + abs(center_y - y)
+                if dist != 0 and dist < min_distance:
                     min_distance = dist
     print(min_distance)
