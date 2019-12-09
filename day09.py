9bb0369f66d1d3d370eb1fda6611fd369f12019d
spenszor <slawosz.penszor@gmail.com>
2019-12-09 19:52:22 +0100

    Day Nine

---
+++ day09.py
@@ -0,0 +1,121 @@
+from itertools import permutations
+
+INSTRUCTION_POINTER_MAPPING = [-1, +4, +4, +2, +2, 0, 0, +4, +4, +2]
+
+class Computer:
+    def __init__(self, memory):
+        self.memory = memory.copy()
+        self.current_pos = 0
+        self.halted = False
+        self.last_output = 0
+        self.relative_base = 0
+
+    def calculate(self, param = None):
+        while True:
+            raw_instr = self.memory[self.current_pos]
+            proc = str(raw_instr).zfill(5)
+
+            instr = int(proc[-2:])
+            modes = list(map(lambda x: int(x), proc[:-2]))
+            # print(f'Performing {instr}, modes: {modes}')
+            if instr == 1:
+                v1 = self.read_by_mode(modes[-1], self.current_pos + 1)
+                v2 = self.read_by_mode(modes[-2], self.current_pos + 2)
+                res = v1 + v2
+                self.write_by_mode(modes[-3], self.current_pos + 3, res)
+            elif instr == 2:
+                v1 = self.read_by_mode(modes[-1], self.current_pos + 1)
+                v2 = self.read_by_mode(modes[-2], self.current_pos + 2)
+                res = v1 * v2
+                self.write_by_mode(modes[0], self.current_pos + 3, res)
+            elif instr == 3:
+                if param:
+                    self.write_by_mode(modes[-1], self.current_pos + 1, param)
+                else:
+                    read_v = input('Give param: ')
+                    self.write_by_mode(modes[-1], self.current_pos + 1, int(read_v))
+            elif instr == 4:
+                v = self.read_by_mode(modes[-1], self.current_pos + 1)
+                self.last_output = v
+                print(f'Instr 4 triggered: {v}')
+            elif instr == 5:
+                v1 = self.read_by_mode(modes[-1], self.current_pos + 1)
+                new_ptr = self.read_by_mode(modes[-2], self.current_pos + 2)
+                if v1 != 0:
+                    self.current_pos = new_ptr
+                else:
+                    self.current_pos += 3
+            elif instr == 6:
+                v1 = self.read_by_mode(modes[-1], self.current_pos + 1)
+                new_ptr = self.read_by_mode(modes[-2], self.current_pos + 2)
+                if v1 == 0:
+                    self.current_pos = new_ptr
+                else:
+                    self.current_pos += 3
+            elif instr == 7:
+                v1 = self.read_by_mode(modes[-1], self.current_pos + 1)
+                v2 = self.read_by_mode(modes[-2], self.current_pos + 2)
+                if v1 < v2:
+                    self.write_by_mode(modes[0], self.current_pos + 3, 1)
+                else:
+                    self.write_by_mode(modes[0], self.current_pos + 3, 0)
+            elif instr == 8:
+                v1 = self.read_by_mode(modes[-1], self.current_pos + 1)
+                v2 = self.read_by_mode(modes[-2], self.current_pos + 2)
+                if v1 == v2:
+                    self.write_by_mode(modes[0], self.current_pos + 3, 1)
+                else:
+                    self.write_by_mode(modes[0], self.current_pos + 3, 0)
+            elif instr == 9:
+                new_base = self.read_by_mode(modes[-1], self.current_pos +1)
+                self.relative_base += new_base
+                # print(f'Relative base changed to {self.relative_base}')
+            elif instr == 99:
+                self.halted = True
+                break
+            self.current_pos += INSTRUCTION_POINTER_MAPPING[instr]
+
+    def read_by_mode(self, mode, address):
+        if mode == 2:
+            check_and_extend_arr(self.memory, self.memory[address] + self.relative_base)
+            return self.memory[self.memory[address] + self.relative_base]
+        elif mode == 1:
+            check_and_extend_arr(self.memory, address)
+            return self.memory[address]
+        elif mode == 0:
+            check_and_extend_arr(self.memory, address)
+            addr = self.memory[address]
+            check_and_extend_arr(self.memory, addr)
+            return self.memory[addr]
+
+    def write_by_mode(self, mode, address, value_to_write):
+        if mode == 2:
+            check_and_extend_arr(self.memory, self.memory[address] + self.relative_base)
+            self.memory[self.memory[address] + self.relative_base] = value_to_write
+        elif mode == 1:
+            check_and_extend_arr(self.memory, address)
+            self.memory[address] = value_to_write
+        elif mode == 0:
+            check_and_extend_arr(self.memory, address)
+            addr = self.memory[address]
+            check_and_extend_arr(self.memory, addr)
+            self.memory[addr] = value_to_write
+
+def check_and_extend_arr(arr, idx):
+    if idx >= len(arr):
+        arr.extend(((idx + 1) - len(arr)) * [0])
+    return arr
+
+with open('./inputs/day09') as inpt:
+    parsed = list(map(lambda x: int(x), inpt.readline().split(',')))
+    t_seq = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+    t_seq2 = [1102,34915192,34915192,7,4,7,99,0]
+    t_seq3 = [104,1125899906842624,99]
+    # part one
+    c = Computer(parsed)
+    c.calculate()
+    print(c.last_output)
+    # part two
+    c = Computer(parsed)
+    c.calculate(2)
+    print(c.last_output)
