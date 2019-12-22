6ff5012b4799b3d12b83f193352d78b688e12b99
spenszor <slawosz.penszor@gmail.com>
2019-12-22 19:15:22 +0100

    Day 20 part 2

--- day20.py
+++ day20.py
@@ -1,106 +1,96 @@
-from collections import deque, defaultdict
-from pprint import pprint
+def parse(raw_data):
+    n, m = len(raw_data), len(raw_data[0])
+    grid = set() 
+    portals = {}
+    for i in range(n):
+        for j in range(m):
+            if raw_data[i][j] == '.':
+                grid.add((i, j))
+            elif raw_data[i][j].isupper():
+                if i < n-1 and raw_data[i+1][j].isupper():
+                    t = (i-1, j) if raw_data[i-1][j] == '.' else (i+2, j)
+                    if raw_data[i][j]+raw_data[i+1][j] in portals:
+                        portals[raw_data[i][j]+raw_data[i+1][j]].append(t)
+                    else:
+                        portals[raw_data[i][j]+raw_data[i+1][j]] = [t]
+                elif j < m-1 and raw_data[i][j+1].isupper():
+                    t = (i, j-1) if raw_data[i][j-1] == '.' else (i, j+2)
+                    if raw_data[i][j]+raw_data[i][j+1] in portals:
+                        portals[raw_data[i][j]+raw_data[i][j+1]].append(t)
+                    else:
+                        portals[raw_data[i][j]+raw_data[i][j+1]] = [t]
+    return grid, portals
 
+def neighs(p):
+    return [(p[0]+1, p[1]), (p[0], p[1]-1), (p[0]-1, p[1]), (p[0], p[1]+1)]
 
-def print_grid(grid):
-    max_x = max(x for (x, y) in grid.keys()) + 1
-    min_x = min(x for (x, y) in grid.keys())
-    max_y = max(y for (x, y) in grid.keys()) + 1
-    min_y = min(y for (x, y) in grid.keys())
-    for y in range(min_y, max_y):
-        line = ''
-        for x in range(min_x, max_x):
-            line += grid.get((x, y), ' ')
-        print(line)
+def build_graph(floor, gates):
+    graph = {}
+    for n in floor:
+        neigh = []
+        for x in neighs(n):
+            if x in floor:
+                neigh.append(x)
+        for g in gates.values():
+            if len(g) == 2 and n in g:
+                neigh.append(g[(g.index(n)+1) % 2])
+        graph[n] = neigh
+    return graph
 
-
-def bfs_shortest_path(graph, start, goal):
-    explored = []
-    queue = deque([[start]])
+def bfs(graph, start, end):
+    queue = [(start, 0)]
+    seen = set([start])
     while queue:
-        path = queue.popleft()
-        node = path[-1]
-        if node not in explored:
-            neighbours = graph[node]
-            for neighbour in neighbours:
-                new_path = list(path)
-                new_path.append(neighbour)
-                queue.append(new_path)
-                if neighbour == goal:
-                    return new_path
-            explored.append(node)
+        v = queue.pop(0)
+        if v[0] == end:
+            return v[1]
+        for n in graph[v[0]]:
+            if n not in seen:
+                queue.append((n, v[1]+1))
+                seen.add(n)
 
-def move(pos, vec):
-    return (pos[0] + vec[0], pos[1] + vec[1])
+def solve_first(data):
+    grid, portals = parse(data)
+    graph = build_graph(grid, portals)
+    return bfs(graph, portals['AA'][0], portals['ZZ'][0])
 
+def build_graph2(floor, gates, inp):
+    h, w = len(inp), len(inp[0])
+    graph = {}
+    for n in floor:
+        neigh = []
+        for x in neighs(n):
+            if x in floor:
+                neigh.append((x, 0))
+        for g in gates.values():
+            if len(g) == 2 and n in g:
+                if n[0] in [2, h-3] or n[1] in [2, w-3]:
+                    neigh.append((g[(g.index(n)+1) % 2], -1))
+                else:
+                    neigh.append((g[(g.index(n)+1) % 2], 1))
+        graph[n] = neigh
+    return graph
 
-directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+def bfs2(graph, start, end):
+    queue = [(start, 0, 0)]
+    seen = set([(start, 0)])
+    while queue:
+        v = queue.pop(0)
+        if v[0] == end and v[1] == 0:
+            return v[2]
+        for n in graph[v[0]]:
+            if v[1] + n[1] < 0:
+                continue
+            if (n[0], v[1]+n[1]) not in seen:
+                queue.append((n[0], v[1]+n[1], v[2]+1))
+                seen.add((n[0], v[1]+n[1]))
 
-
-def neighbours(x, y):
-    for d in directions:
-        yield (x + d[0], y + d[1])
-
+def solve_second(data):
+    grid, portals = parse(data)
+    graph = build_graph2(grid, portals, data)
+    return bfs2(graph, portals['AA'][0], portals['ZZ'][0])
 
 with open('./inputs/day20') as data:
-    raw_grid = dict()
-    for y, line in enumerate(data):
-        for x, char in enumerate(line.rstrip()):
-            raw_grid[(x, y)] = char
-
-    portals = defaultdict(list)
-    position_to_portal = {}
-    for (x, y), char in raw_grid.items():
-        if char in '.# ':
-            continue
-        pos = None
-        first_letter = char
-        second_letter = None
-        for n in neighbours(x, y):
-            if n not in raw_grid:
-                continue
-            if raw_grid[n] == '.':
-                pos = n
-            elif raw_grid[n] not in '.# ':
-                second_letter = raw_grid[n]
-        if pos is None or second_letter is None:
-            continue
-        [first_letter, second_letter] = list(sorted([first_letter, second_letter]))
-        portals[first_letter + second_letter].append(pos)
-        position_to_portal[pos] = first_letter + second_letter
-    # print_grid(raw_grid)
-    # pprint(portals)
-    # pprint(position_to_portal)
-
-    current_pos = portals['AA'][0]
-    visited_fields = deque()
-    available_grid = dict()
-    adjacency = defaultdict(set)
-    available_grid[current_pos] = '.'
-    while True:
-        for d in directions:
-            new_pos = move(current_pos, d)
-            if new_pos not in available_grid and raw_grid[new_pos] == '.':
-                adjacency[current_pos].add(new_pos)
-                # adjacency[new_pos].add(current_pos)
-                visited_fields.append(current_pos)
-                current_pos = new_pos
-                available_grid[new_pos] = '.'
-                if current_pos in position_to_portal:
-                    temp =  portals[position_to_portal[new_pos]]
-                    telported_to = list(filter(lambda x: x != new_pos, temp))
-                    if telported_to and len(temp) > 1:
-                        adjacency[current_pos].add(telported_to[0])
-                        current_pos = telported_to[0]
-                break
-        else:
-            if visited_fields:
-                current_pos = visited_fields.pop()
-            else:
-                print_grid(available_grid)
-                break
-
-    path = bfs_shortest_path(adjacency, portals['AA'][0], portals['ZZ'][0])
-    print(list(map(lambda x: position_to_portal[x] if x in position_to_portal else x ,path)))
-    # print(path)
-    print(len(path))
+    raw_input = data.read().split('\n')[:-1]
+    print(f'Solution one: {solve_first(raw_input)}')
+    print(f'Solution two: {solve_second(raw_input)}')
